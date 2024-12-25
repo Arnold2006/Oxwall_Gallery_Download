b32926f4c2e23737cc30f4833a1f5df71d9de6ad
Arnold2006 <115367701+Arnold2006@users.noreply.github.com>
2024-12-25 11:59:04 +0100

    Update event_handler.php

--- classes/event_handler.php
+++ classes/event_handler.php
@@ -1,10 +1,10 @@
 <?php
 
-class SIMPLETEST_CLASS_EventHandler
+class ALBUMDOWNLOAD_CLASS_EventHandler
 {
     public function init()
     {
-        OW::getLanguage()->addKeyForJs('simpletest', 'menu_item');
-        OW::getLanguage()->importPluginLangs(OW::getPluginManager()->getPlugin('simpletest')->getRootDir() . 'langs.zip');
+        OW::getLanguage()->addKeyForJs('albumdownload', 'menu_item');
+        OW::getLanguage()->importPluginLangs(OW::getPluginManager()->getPlugin('albumdownload')->getRootDir() . 'langs.zip');
     }
 }
