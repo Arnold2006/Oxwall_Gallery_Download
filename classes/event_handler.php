f63de14d09b1679795cf366189bfb78a9f9510c9
Arnold2006 <115367701+Arnold2006@users.noreply.github.com>
2024-12-25 12:34:56 +0100

    Update event_handler.php

--- classes/event_handler.php
+++ classes/event_handler.php
@@ -5,6 +5,6 @@ class ALBUMDOWNLOAD_CLASS_EventHandler
     public function init()
     {
         OW::getLanguage()->addKeyForJs('albumdownload', 'menu_item');
-        OW::getLanguage()->importPluginLangs(OW::getPluginManager()->getPlugin('albumdownload')->getRootDir() . 'langs.zip');
+        OW::getLanguage()->importPluginLangs(OW::getPluginManager()->getPlugin('albumdownload')->getRootDir() . 'langs.zip', 'albumdownload');
     }
 }
