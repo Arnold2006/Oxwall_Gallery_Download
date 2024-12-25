0c2ce24718f1dead6b5ce43123bfd3f49e031c73
Arnold2006 <115367701+Arnold2006@users.noreply.github.com>
2024-12-25 12:55:35 +0100

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,4 +1,11 @@
 <?php
 
-$navigation = OW::getNavigation();
-$navigation->deleteMenuItem('albumdownload.index');
+try {
+    $navigation = OW::getNavigation();
+    $navigation->deleteMenuItem('albumdownload.index');
+    
+    $languageService = BOL_LanguageService::getInstance();
+    $languageService->deletePrefix('albumdownload');
+} catch (Exception $e) {
+    OW::getLogger()->addEntry(json_encode($e));
+}
